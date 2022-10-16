8534aa5ff53774af6f9d321b9c54493b62f87a1c
Ashish Mehra <sdashish.am@gmail.com>
2022-10-16 10:39:38 +0530

    Multiple url intent filters

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,49 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {Calendar} from 'react-native-calendars';
 
 const HomeScreen = () => {
+  const [showCalendar, setShowCalendar] = useState(false);
+  const navigation = useNavigation();
+  const openWebView = () => navigation.navigate('Profile');
+
+  const openCalendar = () => {
+    setShowCalendar(true);
+  };
+
+  const hideCalendar = () => {
+    setShowCalendar(false);
+  };
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#222',
+      }}>
+      <Pressable onPress={openWebView}>
+        <Text>Open WebView</Text>
+      </Pressable>
+      <Pressable style={{marginTop: 25}} onPress={openCalendar}>
+        <Text>Open Calendar</Text>
+      </Pressable>
+      {showCalendar && (
+        <View
+          style={{
+            ...StyleSheet.absoluteFillObject,
+            backgroundColor: 'rgba(0,0,0,0.5)',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Calendar />
+          <Pressable style={{marginTop: 25}} onPress={hideCalendar}>
+            <Text>❌ Close Calendar</Text>
+          </Pressable>
+        </View>
+      )}
     </View>
   );
 };
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,14 +1,31 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import {useRoute} from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import {WebView} from 'react-native-webview';
 
 const ProfileScreen = () => {
   const route = useRoute();
-  console.log({params: route.params});
+  const navigation = useNavigation();
+
+  const js = `(function() {
+    //  window.localStorage.setItem('Token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjU3NCwicm9sZSI6IjExIiwiZm5hbWUiOiJBbnNoaWthIiwibG5hbWUiOiJHYW5kaGkiLCJlbWFpbCI6ImFuc2hpa2EzMUBnbWFpbC5jb20iLCJvaWQiOjQyMSwic2Vzc2lvbklkIjoiN2RlYWZjYWM4NWZhNDlkZjQ5MDBmOGRhN2E1Zjg1YjliOWI4YjUyMDVmMDQ2ZDg3MGY5N2UwYTUwNGVmNTdkOCIsImlhdCI6MTY2NTU2ODYxNywiZXhwIjoxNjY4MTYwNjE3fQ.87j4hpiFlyS_Tn2A7GhuwUKMEk_ga6SQaF8YE0c01yQ');
+    //  window.ReactNativeWebView.postMessage(JSON.stringify(event));
+  })();`;
 
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={{flex: 1}}>
+      <WebView
+        source={{
+          uri: 'https://timer-app-omega.vercel.app/',
+        }}
+        onMessage={event => {
+          alert(event.nativeEvent.data);
+          /* event.nativeEvent.data must be string, i.e. window.postMessage
+          should send only string.
+          * */
+        }}
+        // injectedJavaScript={js}
+      />
     </View>
   );
 };
